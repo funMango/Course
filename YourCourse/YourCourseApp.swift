1e4eb7e95c28e4ba1e24e3c2d1a01375a508bfb3
funMango <138420832+funMango@users.noreply.github.com>
2024-03-09 12:10:29 +0900

    first commit

---
+++ YourCourse/YourCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  YourCourseApp.swift
+//  YourCourse
+//
+//  Created by 이민호 on 3/9/24.
+//
+
+import SwiftUI
+
+@main
+struct YourCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
